1cb1ef28a79678459e0a176c073dfb2faa506f0b
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-07-23 16:11:23 +0700

    Open new link in current tab

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+// _blank: open new tab
+// _top: open current tab
 var vueInstance = new Vue({
   el: '#app',
   data: {
     title: 'Samsung smartphone',
-    url: 'https://google.com'
+    url: 'https://google.com',
+    target: '_blank' // _top
   },
   methods: {
     say: function (text) {
